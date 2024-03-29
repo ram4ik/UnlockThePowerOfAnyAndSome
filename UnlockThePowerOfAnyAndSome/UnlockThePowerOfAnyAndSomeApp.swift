e6427a09a0651cf8930d0a17e9f7f6029d8fb1f4
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-03-29 19:10:01 +0200

    Initial Commit

---
+++ UnlockThePowerOfAnyAndSome/UnlockThePowerOfAnyAndSomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnlockThePowerOfAnyAndSomeApp.swift
+//  UnlockThePowerOfAnyAndSome
+//
+//  Created by Ramill Ibragimov on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct UnlockThePowerOfAnyAndSomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
